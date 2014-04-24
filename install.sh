48bfe8676951c34e57df553dbeab5d5fb926bd70
Jirka Lewandowski <jirka.lewandowski@posteo.de>
2014-04-24 21:58:32 +0200

    Install script works without sudo

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 DATADIR=~/.mdma
-EXECDIR=/usr/bin
+EXECDIR=./
 
 mkdir -p ${DATADIR}
 cp markmath.py ${DATADIR}
 cp template.tex ${DATADIR}
 
-sudo -k bash << EOF
+bash << EOF
 echo "MDPATH=$DATADIR" > ${EXECDIR}/mathdown
 echo '\${MDPATH}/markmath.py \$1.mdma > \$1.mdtmp' >> ${EXECDIR}/mathdown
 echo 'pandoc -f markdown --template=\${MDPATH}/template.tex -s -o \$1.tex \$1.mdtmp' >> ${EXECDIR}/mathdown
@@ -14,3 +14,4 @@ echo 'pandoc -f markdown --template=\${MDPATH}/template.tex -s -o \$1.pdf \$1.md
 echo 'rm \$1.mdtmp' >> ${EXECDIR}/mathdown
 chmod +x ${EXECDIR}/mathdown
 EOF
+sudo mv mathdown /usr/bin/
